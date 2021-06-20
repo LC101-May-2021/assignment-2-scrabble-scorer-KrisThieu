cb70fdc259aa00de472dee7d89d16e74d1d55247
Kris Thieu <84294025+KrisThieu@users.noreply.github.com>
2021-06-20 21:08:18 +0000

    seventh submission

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -81,17 +81,17 @@ function scrabbleScore(word) {
 }
 
 const scoringAlgorithms = [
-  { name: 'Simple Score',
+  Object({ name: 'Simple Score',
   description: 'Each letter is worth 1 point.',
-  scoreFunction: 'simpleScore' }, 
+  scoreFunction: 'simpleScore' }), 
   
-  { name: 'Bonus Vowels',
+  Object({ name: 'Bonus Vowels',
   description: 'Vowels are 3 pts, consonants are 1 pt.',
-  scoreFunction: 'vowelBonusScore' }, 
+  scoreFunction: 'vowelBonusScore' }), 
   
-  { name: 'Scrabble',
+  Object({ name: 'Scrabble',
   description: 'The traditional scoring algorithm.',
-  scoreFunction: 'scrabbleScore' }
+  scoreFunction: 'scrabbleScore' })
 ];
 
 function scorerPrompt(word) {
